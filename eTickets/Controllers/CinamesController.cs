2c17fc8f57a5f920884a41b450001a087fea1718
Olaniyi489 <olawareez96@gmail.com>
2022-12-10 23:31:06 +0100

    intial commit

---
+++ eTickets/Controllers/CinamesController.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class CinamesController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CinamesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task <IActionResult> Index()
+        {
+            var allCiname = await _appDbContext.Cinema.ToListAsync();
+            return View(allCiname);
+        }
+    }
+}
---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProducersController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var producers = await _appDbContext.Producers.ToListAsync();
+            return View(producers);
+        }
+    }
+}
